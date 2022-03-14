1e3a5673c099c6aad6accbe14696b8bf3bb541a6
JiwonHwang <cutty82432@gmail.com>
2022-03-14 21:51:48 +0900

    Try 1931

--- BAEKJOON/그리디/1931.py
+++ BAEKJOON/그리디/1931.py
@@ -4,22 +4,30 @@ import time
 read = sys.stdin.readline
 
 count = int(read())
-start = time.time()
-time_list = [[0]]*count
-res_list = [[0]]* count
-result = 0
+start_time = time.time()
+time_list = [[0,0] for _ in range(count)]
+res_list = [-1, -1]
+res = 0
 
 for i in range(count):
     time_list[i] = list(map(int,read().split()))
 
 time_list.sort(key =lambda x: x[0])
 
+for times in time_list:
+    print(times, res_list, res)
+    if times[0] > res_list[1]:
+        res_list = times
+        res += 1 
+        continue
+    if times[1] < res_list[1]:
+        res_list = times
+    
+print(res)
 
 
 
 
-
-
-
-end=time.time()
-print(end-start)
+#print(time_list)
+end_time=time.time()
+print(end_time-start_time)
