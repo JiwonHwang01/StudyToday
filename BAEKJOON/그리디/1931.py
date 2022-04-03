2dd3622c0ca1b3800204cd05bc73f1f48ba83c09
JiwonHwang <cutty82432@gmail.com>
2022-04-03 21:54:20 +0900

    Solve 1931

--- BAEKJOON/그리디/1931.py
+++ BAEKJOON/그리디/1931.py
@@ -16,18 +16,14 @@ time_list.sort(key =lambda x: x[0])
 
 for times in time_list:
     print(times, res_list, res)
-    if times[0] > res_list[1]:
+    if times[0] >= res_list[1]:
         res_list = times
         res += 1 
         continue
     if times[1] < res_list[1]:
+        if times[0] == times[1] and times[0] == res_list[0]:
+            res += 1
         res_list = times
     
 print(res)
 
-
-
-
-#print(time_list)
-end_time=time.time()
-print(end_time-start_time)
