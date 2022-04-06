0ce5e8b3bb72dfcc25b430b32584497e0ff9ef89
JiwonHwang <cutty82432@gmail.com>
2022-04-06 21:35:14 +0900

    Add 1476

--- BAEKJOON/그리디/1744.py
+++ BAEKJOON/그리디/1744.py
@@ -4,7 +4,7 @@ read = sys.stdin.readline
 sum, pos = 0, 0
 
 count = int(read())
-num_list = [0] * (count+1)
+num_list = [0] * count
 
 for i in range(count):
     num_list[i] = int(read())
@@ -12,8 +12,10 @@ for i in range(count):
 num_list.sort()
 print(num_list)
 for i in range(count):
-    if num_list[i] > 1000:
-        print(sum)
+    if count == 1:
+        sum = num_list[i]
+        break
+    if num_list[i] > 1000 :
         continue
     
     if num_list[i] < 0 :
@@ -40,6 +42,6 @@ for i in range(count):
         num_list[i+1] = 10000
         print(sum)
         print(num_list)
-
+    
 
 print(sum)
