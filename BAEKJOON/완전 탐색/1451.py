eb48c12ed2d93db9e17c8c5e80886ab728375098
JiwonHwang <cutty82432@gmail.com>
2022-04-13 03:07:40 +0900

    Add 1451 9095

--- BAEKJOON/완전 탐색/1451.py
+++ BAEKJOON/완전 탐색/1451.py
@@ -1,5 +1,13 @@
 import sys
 
+def sum_double_array(arr, x1, x2, y1, y2):
+    sum = 0
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            sum += arr[i][j]
+    
+    return sum
+
 read = sys.stdin.readline
 
 n, m = map(int,read().split())
@@ -11,14 +19,45 @@ for i in range(n):
         arr[i][j] = int(line[j])
         sum += int(line[j])
 
-avg = sum / (m*n)
+res = 0
+for i in range(0,n):
+    for j in range(0,m):
+        a, b, c = 0, 0, 0
 
-for i in range(1,n):
-    for j in range(1,m):
-        a, b, c = 0
-        if i < n and j < m :
-            for k in range(0,i+1):
-                for l in range(0,j+1):
-                    a += arr[k][l]
+        if i < n-1 and j < m-1:
+            a = sum_double_array(arr,0,j,0,i)
+            b = sum_double_array(arr,j,m,0,i)
+            c = sum_double_array(arr,0,j,i,n)
+            if b >= c : c += sum_double_array(arr,j,m,i,n)
+            else : b += sum_double_array(arr,j,m,i,n)
+            if res < a*b*c:
+                    res = a*b*c
+                
+        elif j == m-1:
+            a = sum_double_array(arr,0,m,0,i)
+            for k in range(0, m):
+                b = sum_double_array(arr,0,k,i,n) 
+                c = sum_double_array(arr,k,m,i,n)
+                if res < a*b*c:
+                    res = a*b*c
             
-            for k in range(i+1,n):
+            for l in range(i+1, n):
+                b = sum_double_array(arr,0,m,i,l)
+                c = sum_double_array(arr,0,m,l,n)
+                if res < a*b*c:
+                    res = a*b*c
+            
+        elif i == n-1:    
+            a = sum_double_array(arr,0,j,0,n)
+            for k in range(j+1, m):
+                b = sum_double_array(arr,j,k,0,n) 
+                c = sum_double_array(arr,k,m,0,n)
+                if res < a*b*c:
+                    res = a*b*c
+            
+            for l in range(0, n):
+                b = sum_double_array(arr,j,m,0,l)
+                c = sum_double_array(arr,j,m,l,n)
+                if res < a*b*c:
+                    res = a*b*c
+print(res)
