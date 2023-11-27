1fb78640db46c49b0382ea810553fcb57e7dca30
Hwang Jiwon <72727113+JiwonHwang01@users.noreply.github.com>
2023-11-27 15:10:00 +0900

    [Gold IV] Title: 거짓말, Time: 72 ms, Memory: 34104 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+read = sys.stdin.readline
+
+n, m = map(int, read().split())
+know = list(map(int, read().split()))
+num_know = know[0]
+bfs = deque()
+
+if num_know:
+    for num in know[1:]:
+        bfs.append(num)
+
+graph = []
+visited = [False]* (1+n)
+visited_party = [False] * m
+res = 0
+
+for i in range(m):
+    party = list(map(int, read().split()))
+    graph.append(party[1:])
+
+while bfs:
+    node = bfs.popleft()
+    visited[node] = True
+
+    for i in range(m):
+        if node in graph[i]:
+            for num in graph[i]:
+                if not visited[num]:
+                    bfs.append(num)
+            visited_party[i] = True
+                
+for i in range(m):
+    if not visited_party[i]:
+        res += 1
+# print(visited_party)
+# print(visited)
+print(res)
