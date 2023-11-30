091939bd886f74ba1e7997f76d67cc5818c08976
Hwang Jiwon <72727113+JiwonHwang01@users.noreply.github.com>
2023-11-30 09:10:29 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 315.97 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -0,0 +1,14 @@
+def solution(sequence):
+    answer = 0
+    pulse_seq = [sequence[i]*((-1)**i) for i in range(len(sequence))]
+    partial_sum = [0]*len(sequence)
+    partial_sum[0] = pulse_seq[0]
+    
+    for i in range(1, len(pulse_seq)):
+        partial_sum[i] = partial_sum[i-1] + pulse_seq[i] 
+    partial_sum = [0]+partial_sum
+    answer = max(partial_sum)-min(partial_sum)
+    #print(partial_sum)
+    
+    
+    return answer
