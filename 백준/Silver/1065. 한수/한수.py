2f7287794ec1c23099af84b65c58519fbb470c30
Hwang Jiwon <72727113+JiwonHwang01@users.noreply.github.com>
2023-12-08 16:24:30 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,14 @@
+n = input()
+count = 99
+
+if int(n) <= 99:
+        count = n
+
+else:
+    for i in range(100,int(n)+1):
+        nums = list(map(int,list(str(i))))
+
+        if nums[0] + nums[2] == 2* nums[1]:
+            count += 1
+    
+print(count)
